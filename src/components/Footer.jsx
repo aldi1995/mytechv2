4d3944ac4906a3cbfafc8538361df6a5e04c16d3
aldi1995 <108751263+aldi1995@users.noreply.github.com>
2025-10-26 17:49:49 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -102,12 +102,12 @@ const Footer = () => {
               <li className="flex items-start text-slate-300 dark:text-slate-400">
                 <MapPin className="w-5 h-5 mr-3 text-sky-400 mt-1" />
                 <a
-                  href="https://www.google.com/maps?q=Jl.+Cipedes+Tengah+No.35,+Sukagalih,+Sukajadi,+Bandung,+Jawa+Barat+40163"
+                  href="#"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="hover:text-sky-400 transition-colors duration-200 leading-relaxed"
                 >
-                  Jl. Cipedes Tengah No.35, Kelurahan Sukagalih, Kecamatan Sukajadi, <br />
+                  Jl. Asia Afrika No.57, Braga, Kec. Sumur Bandung, Kota Bandung, Jawa Barat 40251 Menara BRI Lantai 6 <br />
                   Kota Bandung, Jawa Barat 40163
                 </a>
               </li>
